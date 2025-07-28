e399a76d05632bf878b097483e4ea8f083170e10
tejasAppetals <tejas.talkar@appetals.com>
2025-07-28 17:00:06 +0530

    create screen of theme

--- src/app/dashboard/settings/themes/themes.component.ts
+++ src/app/dashboard/settings/themes/themes.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-themes',
-  templateUrl: './themes.component.html',
-  styleUrls: ['./themes.component.scss']
+  selector: "app-themes",
+  templateUrl: "./themes.component.html",
+  styleUrls: ["./themes.component.scss"],
 })
-export class ThemesComponent {}
+export class ThemesComponent {
+  selectedTheme: string | null = null;
+
+  onThemeChange(value: string) {
+    this.selectedTheme = value;
+    // Set Ionic primary color and header background
+    document.documentElement.style.setProperty('--ion-color-primary', value);
+    document.documentElement.style.setProperty('--ion-color-primary-rgb', this.hexToRgb(value));
+    document.documentElement.style.setProperty('--background', value);
+    // You can add more variables for sidebar/menu/icon as needed
+  }
+
+  // Helper to convert hex to rgb string
+  hexToRgb(hex: string): string {
+    let c = hex.replace('#', '');
+    if (c.length === 3) c = c.split('').map(x => x + x).join('');
+    const num = parseInt(c, 16);
+    return `${(num >> 16) & 255},${(num >> 8) & 255},${num & 255}`;
+  }
+}
