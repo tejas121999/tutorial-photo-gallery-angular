4d558d6ef2fcb08ba74aa97c148412c1e2dae889
tejasAppetals <tejas.talkar@appetals.com>
2025-08-27 17:47:30 +0530

    try to make build

--- android/app/src/main/java/io/ionic/demo/pg/cap/ng/MainActivity.java
+++
@@ -1,7 +0,0 @@
-package io.ionic.demo.pg.cap.ng;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {
-	// Default BridgeActivity - no custom status/navigation bar handling
-}
